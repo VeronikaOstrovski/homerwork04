36e21652cde9d21734336320c94b23d004aa56dd
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-29 09:30:00 +0300

    Task 1 and task 2 done

---
+++ src/CalcMain.java
@@ -0,0 +1,11 @@
+public class CalcMain {
+
+    public static void main (String[] args) {
+
+        Calculator calculator = new Calculator();
+        int sum = calculator.addition(5, 36);
+        boolean greater = calculator.isGreater(12.0, 25.0);
+    }
+
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.startEngine();
+        car.stopEngine();
+
+        car.carBrand = "Honda";
+        car.carModel = "CR-V";
+        car.carYear = 2023;
+
+    }
+
+}
+
+
+
+
